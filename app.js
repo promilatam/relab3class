d6afb596c7db47ffd74b20332f6c214f64828cd3
Manish KC <kcmanish89@gmail.com>
2019-06-06 15:28:19 -0700

    Pair programming function fix

--- app.js
+++ app.js
@@ -1,219 +1,94 @@
-
-var emptyArray = [];
-var quizAnswers = ['a', 'b', 'c', 'd', 'e'];
-var arrayOfThings = ['string', {}, ['thing'], console.log, undefined, null, 0, 2, true, 'last item of the array'];
-
-var aboutme = prompt('I\m from China?')
-
-console.log(aboutme);
-console.log('aboutme[0]')
-
-// If they say 'YES' or 'NO'
-
-if (aboutme === 'yes' || aboutme === 'y' || aboutme ==='yes') {
-alert('that\s right you rock it');
-} else if (abou'use strict'
+'use strict';
 
 
-// var emptyArray = [];
-// var quizAnswers = ['a', 'b', 'c', 'd', 'e'];
-// var arrayOfThings = ['string', {}, ['thing'], console.log, undefined, null, 0, 2, true, 'last item of the array'];
+//function one
+var aboutme = function() {
+  var first = prompt('Am I from China?').toLowerCase();
+  if (first === 'no' || first === 'n') {
+    alert('You got it wrong');
+  }
+  else if (first === 'yes' || first === 'y') {
+    alert('You are my friend!');         
+  } else {
+    alert('Invalid input');
+  }
+ 
+};
 
-// var aboutme = prompt('I\m from China?')
-
-// console.log(aboutme);
-// console.log('aboutme[0]')
-
-// // If they say 'YES' or 'NO'
-
-// if (aboutme === 'yes' || aboutme === 'y' || aboutme ==='yes') {
-// alert('that\s right you rock it');
-// } else if (aboutme === 'no'){
-// alert('we you bad, you have one more chance.');
-// }else {
-// alert('you have not tried at all');
-// }
+//calling function one
+aboutme();
 
 
-// var country = prompt('I have a son?')
-// console.log(country);
-// console.log('country[0]')
-// if (country === 'yes' || country === 'y' || country ==='yes') {
-// alert('that\s right you rock it');
-// } else if (country === 'no'){
-// alert('we you bad, you have one more chance.');
-// }else {
-// alert('you have not tried at all');
-// }
 
 
+var numGuess = function() {
 //Generate random number from 1-100
-var randomNumber = Math.floor(Math.random() * 100) + 1;
+  var randomNumber = Math.floor(Math.random() * 100) + 1;
 
-console.log(randomNumber);
-var sixthResponse = parseInt(prompt('Guess the number I am thinking in between 1-100'));
+  console.log(randomNumber);
+  var sixthResponse = parseInt(prompt('Guess the number I am thinking in between 1-100'));
 
-console.log(sixthResponse);
-console.log(typeof(sixthResponse));
+  console.log(sixthResponse);
+  console.log(typeof(sixthResponse));
 
-for (var i = 0; i < 4; i++) {
-if (sixthResponse === randomNumber) {
-console.log('correct guess');
-alert('correct!!');
-break;
+  for (var i = 0; i < 4; i++) {
+    if (sixthResponse === randomNumber) {
+      console.log('correct guess');
+      alert('correct!!');
+      break;
 
-}
-else if (sixthResponse > randomNumber) {
-sixthResponse = parseInt(prompt('Too high. Guess again!'));
-console.log('Too High!');
-}
-else {
-sixthResponse = parseInt(prompt('Too low. Guess again!'));
-console.log('Too low!');
-}
-}
-// 7 Qustion
-// Crearing Arrey called 'statevisted' which has list of states
+    }
+    else if (sixthResponse > randomNumber) {
+      sixthResponse = parseInt(prompt('Too high. Guess again!'));
+      console.log('Too High!');
+    }
+    else {
+      sixthResponse = parseInt(prompt('Too low. Guess again!'));
+      console.log('Too low!');
+    }
+  }
+};
 
-var stateVisited = new Array( 'New York', 'Texas', 'New Jeersey', 'Ohio', 'Utah','Maine', 'Michigan');
-console.log('stateVisited:' + stateVisited);
-prompt('Let\s play guessing game');
-
-var choice = prompt('which state I have visited?');
-console.log('choice:' + choice);
-
-var attempt = 1;
-loop:
-while(attempt < 6)
-{
-for (var i = 0; i < stateVisited.length; i++){
-if(choice === stateVisited[i]){
-alert('Yes! you are rock star! thats is right!')
-braek
-loop1;
-
-}
-
-}
-choice = prompt('You all most got this, Let\s try it again');
-attempt++;
-console.log('choice: ' + choice);
-
-}
+numGuess();
 
 
 
 
-'use strict'
-
-
-// var emptyArray = [];
-// var quizAnswers = ['a', 'b', 'c', 'd', 'e'];
-// var arrayOfThings = ['string', {}, ['thing'], console.log, undefined, null, 0, 2, true, 'last item of the array'];
-
-// var aboutme = prompt('I\m from China?')
-
-// console.log(aboutme);
-// console.log('aboutme[0]')
-
-// // If they say 'YES' or 'NO'
-
-// if (aboutme === 'yes' || aboutme === 'y' || aboutme ==='yes') {
-// alert('that\s right you rock it');
-// } else if (aboutme === 'no'){
-// alert('we you bad, you have one more chance.');
-// }else {
-// alert('you have not tried at all');
 // }
+// // 7 Qustion
+// // Crearing Arrey called 'statevisted' which has list of states
 
+var stateGuess = function() {
+  var stateVisited = new Array( 'new york', 'texas', 'new jersey', 'ohio', 'utah','maine', 'michigan');
+  console.log('stateVisited:' + stateVisited);
+  prompt('Let\s play guessing game');
 
-// var country = prompt('I have a son?')
-// console.log(country);
-// console.log('country[0]')
-// if (country === 'yes' || country === 'y' || country ==='yes') {
-// alert('that\s right you rock it');
-// } else if (country === 'no'){
-// alert('we you bad, you have one more chance.');
-// }else {
-// alert('you have not tried at all');
-// }
+  var choice = prompt('which state I have visited?');
+  console.log('choice:' + choice);
 
+  var attempt = 1;
+  loop:
+  while(attempt < 6)
+  {
+    for (var i = 0; i < stateVisited.length; i++){
+      if(choice === stateVisited[i]){
+        alert('Yes! you are rock star! thats is right!');
+        break loop;
 
-//Generate random number from 1-100
-var randomNumber = Math.floor(Math.random() * 100) + 1;
+      }
 
-console.log(randomNumber);
-var sixthResponse = parseInt(prompt('Guess the number I am thinking in between 1-100'));
+    }
+    choice = prompt('You all most got this, Let\s try it again');
+    attempt++;
+    console.log('choice: ' + choice);
 
-console.log(sixthResponse);
-console.log(typeof(sixthResponse));
+  }
+};
 
-for (var i = 0; i < 4; i++) {
-if (sixthResponse === randomNumber) {
-console.log('correct guess');
-alert('correct!!');
-break;
-
-}
-else if (sixthResponse > randomNumber) {
-sixthResponse = parseInt(prompt('Too high. Guess again!'));
-console.log('Too High!');
-}
-else {
-sixthResponse = parseInt(prompt('Too low. Guess again!'));
-console.log('Too low!');
-}
-}
-// 7 Qustion
-// Crearing Arrey called 'statevisted' which has list of states
-
-var stateVisited = new Array( 'New York', 'Texas', 'New Jeersey', 'Ohio', 'Utah','Maine', 'Michigan');
-console.log('stateVisited:' + stateVisited);
-prompt('Let\s play guessing game');
-
-var choice = prompt('which state I have visited?');
-console.log('choice:' + choice);
-
-var attempt = 1;
-loop:
-while(attempt < 6)
-{
-for (var i = 0; i < stateVisited.length; i++){
-if(choice === stateVisited[i]){
-alert('Yes! you are rock star! thats is right!')
-braek
-loop1;
-
-}
-
-}
-choice = prompt('You all most got this, Let\s try it again');
-attempt++;
-console.log('choice: ' + choice);
-
-}
+stateGuess();
 
 
 
-
-
-tme === 'no'){
-alert('we you bad, you have one more chance.');
-}else {
-alert('you have not tried at all');
-}
-
-
-var country = prompt('I have a son?')
-console.log(country);
-console.log('country[0]')
-if (country === 'yes' || country === 'y' || country ==='yes') {
-alert('that\s right you rock it');
-} else if (country === 'no'){
-alert('we you bad, you have one more chance.');
-}else {
-alert('you have not tried at all');
-}
 
 
 
