3048793d597d11c938ea0a7a4bda4d13ad14052b
Promila Tamanggit config --global user.email promilatam@gmail.com <YOUR EMAIL ADDRESS>
2019-06-13 16:03:43 -0700

    redone

--- app.js
+++ app.js
@@ -1,23 +1,96 @@
 'use strict';
 
+var userName = prompt('What is your name?');
+var first = 'Hey ' + userName + ' Welcome to my page.';
+alert(first);
 
-//function one
-var aboutme = function() {
+
+//function for Quswer one
+var aboutmeQuswer1 = function() {
   var first = prompt('Am I from China?').toLowerCase();
   if (first === 'no' || first === 'n') {
-    alert('You got it wrong');
+    alert('You are my friend! ' + userName );
   }
   else if (first === 'yes' || first === 'y') {
-    alert('You are my friend!');         
+    alert(userName + ' You got it wrong ');         
   } else {
-    alert('Invalid input');
+    alert(userName + ' Its Invalid input');
   }
  
 };
+aboutmeQuswer1();
 
-//calling function one
-aboutme();
+//function for Quswer 2
+var aboutmeQuswer2 = function() {
+  var first = prompt('I have 3 sons?').toLowerCase();
+  if (first === 'no' || first === 'n') {
+    alert('You are my friend! ' + userName );
+  }
+  else if (first === 'yes' || first === 'y') {
+    alert(userName + ' You got it wrong ');         
+  } else {
+    alert(userName + ' Its Invalid input');
+  }
+ 
+};
+aboutmeQuswer2();
 
+//function for Quswer 3
+var aboutmeQuswer3 = function() {
+  var first = prompt('I like Seahawks?').toLowerCase();
+  if (first === 'yes' || first === 'y') {
+    alert('You got it wrong ' + userName );
+  }
+  else if (first === 'no' || first === 'n') {
+    alert(userName + ' You got it got this! ');         
+  } else {
+    alert(userName + ' Its Invalid input');
+  }
+ 
+};
+aboutmeQuswer3();
+
+
+//function for Quswer 4
+var aboutmeQuswer4 = function() {
+  var first = prompt('Do I speak 5 languages?').toLowerCase();
+  if (first === 'yes' || first === 'y') {
+    alert('You got it :(' + userName );
+  }
+  else if (first === 'no' || first === 'n') {
+    alert(userName + ' You got it wrong! ');         
+  } else {
+    alert(userName + ' Its Invalid input');
+  }
+ 
+};
+aboutmeQuswer4();
+
+//function for Quswer 5
+var aboutmeQuswer5 = function() {
+  var first = prompt('Have I visit 15 countries?').toLowerCase();
+  if (first === 'no' || first === 'n') {
+    alert('You got it wrong ' + userName );
+  }
+  else if (first === 'yes' || first === 'y') {
+    alert(userName + ' You got it got this! ');         
+  } else {
+    alert(userName + ' Its Invalid input');
+  }
+ 
+};
+aboutmeQuswer5();
+
+
+
+// //calling function one
+// aboutmeQuswer1();
+
+// // function for the Quswer 2
+// var aboutmeQuswer2 = function(){
+//   var Quswer2 = prompt('How old is my son?').
+
+// }
 
 
 
@@ -61,7 +134,7 @@ numGuess();
 var stateGuess = function() {
   var stateVisited = new Array( 'new york', 'texas', 'new jersey', 'ohio', 'utah','maine', 'michigan');
   console.log('stateVisited:' + stateVisited);
-  prompt('Let\s play guessing game');
+  alert('Let\s play guessing game');
 
   var choice = prompt('which state I have visited?');
   console.log('choice:' + choice);
@@ -71,14 +144,14 @@ var stateGuess = function() {
   while(attempt < 6)
   {
     for (var i = 0; i < stateVisited.length; i++){
-      if(choice === stateVisited[i]){
+      if(choice.toLowerCase() === stateVisited[i].toLowerCase){
         alert('Yes! you are rock star! thats is right!');
         break loop;
 
       }
 
     }
-    choice = prompt('You all most got this, Let\s try it again');
+    choice = prompt('You all most got this, Let\'s try it again');
     attempt++;
     console.log('choice: ' + choice);
 
